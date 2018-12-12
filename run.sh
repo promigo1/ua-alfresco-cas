eb0f04db144622a743d79bba7d7b647751f2ab09
promigo1 <promigo+gi@protonmail.com>
2018-12-12 11:01:13 +0000

    add local profile to run

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#MAVEN_OPTS="-Xms256m -Xmx2G" mvn clean install alfresco:run
-MAVEN_OPTS="-Xms256m -Xmx2G -javaagent:/home/workuser1/apps/hotswapagent/hotswap-agent-1.3.0.jar -XXaltjvm=dcevm" mvn clean install alfresco:run
+MAVEN_OPTS="-Xms256m -Xmx2G" mvn clean install -Plocal alfresco:run
+#MAVEN_OPTS="-Xms256m -Xmx2G -javaagent:/home/workuser1/apps/hotswapagent/hotswap-agent-1.3.0.jar -XXaltjvm=dcevm" mvn clean install -Plocal alfresco:run
